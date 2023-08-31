8b130265179f11b67d4494752f425cbbebbf40d4
Marcos Vinícius <vo7775832@gmail.com>
2023-08-31 02:54:26 -0300

    feat : validacoes e filtragens

--- src/main/java/com/example/jsfcrud/entities/UserEntity.java
+++ src/main/java/com/example/jsfcrud/entities/UserEntity.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Entity
@@ -17,35 +18,64 @@ import java.util.Date;
 @ToString
 @RequiredArgsConstructor
 @Table(name = "users")
-
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
+
     @Basic
-    @Column
-    @NotNull(message = "can't be empty")
-    @NotEmpty(message = "can't be empty")
-    @Email(message = "Invalid Email")
+    @NotNull(message = "O nome não pode estar vazio")
+    @NotEmpty(message = "O nome não pode estar vazio")
+    @Column(length = 50)
+    private String nome;
+
+    @Basic
+    @Column(unique = true,length = 50)
+    @NotNull(message = "O nome de usuário não pode estar vazio")
+    @NotEmpty(message = "O nome de usuário não pode estar vazio")
+    private String username;
+
+    @Basic
+    @NotNull(message = "A senha não pode estar vazia")
+    @NotEmpty(message = "A senha não pode estar vazia")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[A-Z]).{8,}$", message = "A senha deve ter pelo menos 8 caracteres, incluindo um número e uma letra maiúscula")
+    @Column(nullable = false)
+    private String senha;
+
+    @Basic
+    @NotNull(message = "O CEP não pode estar vazio")
+    @NotEmpty(message = "O CEP não pode estar vazio")
+    @Column(length = 8)
+    private String cep;
+
+    @Basic
+    @NotNull(message = "O email não pode estar vazio")
+    @NotEmpty(message = "O email não pode estar vazio")
+    @Email(message = "Email inválido")
+    @Column(unique = true, length = 100)
     private String email;
+
     @Basic
-    @Column
-    @NotNull(message = "can't be empty")
-    @NotEmpty(message = "can't be empty")
-    private String firstname;
-    @Basic
-    @Column
-    @NotNull(message = "can't be empty")
-    @NotEmpty(message = "can't be empty")
-    private String lastname;
-    @Basic
-    @Column
-    @NotNull(message = "can't be empty")
-    @NotEmpty(message = "can't be empty")
-    private String phoneNumber;
-    @Basic
-    @Column
     @Temporal(TemporalType.DATE)
-    @NotNull(message = "can't be empty")
-    private Date dateOfBirth;
+    @NotNull(message = "A data de nascimento não pode estar vazia")
+    @Column(nullable = false)
+    private Date dataNascimento;
+
+    @Basic
+    @NotNull(message = "O CPF/CNPJ não pode estar vazio")
+    @NotEmpty(message = "O CPF/CNPJ não pode estar vazio")
+    @Column(length = 14)
+    private String cpfCnpj;
+
+    @Basic
+    @NotNull(message = "O sexo não pode estar vazio")
+    @NotEmpty(message = "O sexo não pode estar vazio")
+    @Column(nullable = false)
+    private String sexo;
+
+    @Basic
+    @NotNull(message = "O tipo de usuário não pode estar vazio")
+    @NotEmpty(message = "O tipo de usuário não pode estar vazio")
+    @Column(nullable = false)
+    public String tipoUsuario;
 }
--- src/main/java/com/example/jsfcrud/services/UserService.java
+++ src/main/java/com/example/jsfcrud/services/UserService.java
@@ -1,21 +1,34 @@
 package com.example.jsfcrud.services;
 
 import com.example.jsfcrud.entities.UserEntity;
+import com.example.jsfcrud.exception.InvalidAgeException;
 import com.example.jsfcrud.repository.UserRepository;
+import com.example.jsfcrud.utils.UserValidationUtils;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.primefaces.PrimeFaces;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-@RequiredArgsConstructor
+@ManagedBean
 @ViewScoped
-@Service
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class UserService {
+
+    private String mask;
+
+    private String pesquisaNome;
 
     private final UserRepository userRepository;
     @Getter
@@ -23,12 +36,22 @@ public class UserService {
     @Getter
     private UserEntity user = new UserEntity();
 
-    public boolean addUser(){
-        userRepository.save(user);
-        users.add(user);
-        user = new UserEntity();
-        return user.getId() > 0;
+    public void addUser() {
+        try {
+            if (UserValidationUtils.isUserAbove18(user)) {
+                userRepository.save(user);
+                users.add(user);
+                user = new UserEntity();
+            } else {
+                throw new InvalidAgeException("Idade inválida");
+            }
+        } catch (InvalidAgeException e) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário tem que ter idade maior que 18 anos", "Idade inválida"));
+            PrimeFaces.current().executeScript("PF('errorDialog').show();");
+        }
     }
+
     public void updateUser(UserEntity userEntity){
         if(userEntity.getId() == 0) return;
         userRepository.save(userEntity);
@@ -45,6 +68,19 @@ public class UserService {
     @PostConstruct
     public void getAllUser(){
         users = userRepository.findAll();
+    }
+
+    public void searchUsers() {
+        users =  userRepository.searchUsersByName(pesquisaNome);
+    }
+
+    // Método para atualizar a máscara com base no tipo de pessoa selecionado
+    public void updateMask() {
+        if ("fisica".equals(user.getTipoUsuario())) {
+            mask = "999.999.999-99";
+        } else if ("juridica".equals(user.getTipoUsuario())) {
+            mask = "99.999.999/9999-99";
+        }
     }
 
 
